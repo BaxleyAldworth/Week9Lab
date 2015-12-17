6ccc66ea4afbb4bb3365e197f786bdb752fa06d6
Cason Barnhill <casonbarnhill@yahoo.com>
2015-12-17 14:54:20 -0600

    added migrations, added models- user, profile, post

---
+++ Week9Lab/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Week9Lab.Models
+{
+    public class Post
+    {
+
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public string ImageUrl { get; set; }
+        [NotMapped]
+        public DateTime PostDate { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ Week9Lab/Models/Profile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Week9Lab.Models
+{
+    public class Profile
+    {
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public string Website { get; set; }
+        public DateTime DOB { get; set; }
+        public int MyProperty { get; set; }
+        public virtual User Userprofile { get; set; }
+        
+
+    }
+}
